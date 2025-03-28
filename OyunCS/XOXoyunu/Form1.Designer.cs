9778bb5a8a740768313c88360a27a03d788e9ed4
Enes <65052450+enesaltndll@users.noreply.github.com>
2025-03-28 16:38:18 +0300

    Add files via upload

---
+++ OyunCS/XOXoyunu/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace XOXoyunu
+{
+    partial class FrmOyunAlani
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.btn8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(269, 303);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(260, 143);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.generic_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(535, 153);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(260, 143);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.generic_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(269, 153);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(260, 143);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.generic_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(3, 153);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(260, 143);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.generic_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(535, 3);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(260, 143);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.generic_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(269, 3);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(260, 143);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.generic_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(535, 303);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(260, 143);
+            this.btn9.TabIndex = 9;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.generic_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(3, 303);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(260, 143);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.generic_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(3, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(260, 143);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.generic_Click);
+            // 
+            // FrmOyunAlani
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "FrmOyunAlani";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XOX Oyun";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmOyunAlani_KeyDown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+    }
+}
+
---
+++ OyunCS/XOXoyunu/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XOXoyunu
+
+
+{
+    public partial class FrmOyunAlani : Form
+
+    {
+        public FrmOyunAlani()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void FrmOyunAlani_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.Control == true && e.KeyCode == Keys.Enter)
+            {
+                var cevap = MessageBox.Show("Çıkmak İstediğinizde Emin misiniz?", "UYARI", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (cevap == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+            }
+
+        }
+        bool birinciOyuncuMu = true;
+        private void generic_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Text == string.Empty)
+            {
+                if (birinciOyuncuMu == true)
+                {
+                    button.BackColor = Color.Black;
+                    button.ForeColor = Color.Red;
+                    button.Text = "X";
+                    button.Font = new Font("Consolas", 75, FontStyle.Bold);
+                }
+                else
+                {
+                    button.BackColor = Color.Red;
+                    button.ForeColor = Color.Black;
+                    button.Text = "O";
+                    button.Font = new Font("Consolas", 75, FontStyle.Bold);
+
+      
+
+                }
+           
+            }
+            KazandiMi();
+        }
+
+        private void KazandiMi()
+        {
+            if (
+                 (btn1.Text == btn2.Text && btn2.Text == btn3.Text && btn3.Text != string.Empty) ||
+                (btn4.Text == btn5.Text && btn5.Text == btn6.Text && btn6.Text != string.Empty) ||
+                (btn7.Text == btn8.Text && btn8.Text == btn9.Text && btn9.Text != string.Empty) ||
+                
+
+                (btn1.Text == btn4.Text && btn4.Text == btn7.Text && btn7.Text != string.Empty) ||
+                (btn2.Text == btn5.Text && btn5.Text == btn8.Text && btn8.Text != string.Empty) ||
+                (btn3.Text == btn6.Text && btn6.Text == btn9.Text && btn9.Text != string.Empty) ||
+
+                (btn1.Text == btn5.Text && btn5.Text == btn9.Text && btn9.Text != string.Empty) ||
+                (btn3.Text == btn5.Text && btn5.Text == btn7.Text && btn7.Text != string.Empty) 
+                )
+               
+            {
+                if(birinciOyuncuMu)
+                {
+                    MessageBox.Show("1.Oyuncu Kazandı");
+                }
+                else
+                {
+                    MessageBox.Show("2.Oyuncu Kazandı");
+                  
+                }
+                this.Close();
+
+            }
+             birinciOyuncuMu = !birinciOyuncuMu;
+        }
+    }
+}
+
